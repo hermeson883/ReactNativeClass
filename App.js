e61f97ca889fd3497ef934beb722c61dd38993f1
hermeson883 <hermesondovalebeserra722@gmail.com>
2024-03-16 23:55:07 -0300

    login page

--- App.js
+++ App.js
@@ -1,27 +1,44 @@
-import { StyleSheet, View, Text, Button } from 'react-native';
+import {View, Text, TouchableOpacity } from 'react-native';
+import {flexStyleLogin, globalColors,textStyle} from './styles'
+// import { AppleLogo, GoogleLogo } from 'phosphor-react-native';
+
+/* 
+  Breve resumo do React Native
+    View === div 
+    Button === button 
+    Text === p
+    TouchableOpacity === Botão estilizavel <- que desgraça
+
+*/
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Button title='Hermeson' />
-      <Button title='Skills' />
-      <Button title='Languages' />
+    <View style={flexStyleLogin.flexStyleContainer}>
+        <View style={flexStyleLogin.formStyle}>
+          <Text style={[textStyle.bigSize, globalColors.black]}>Explore Agora</Text>
+          <Text style={textStyle.mediumSize }>Junte-se HOJE.</Text>
+          <View>
+            <TouchableOpacity style={flexStyleLogin.loginButtons}>
+              <Text style={flexStyleLogin.textButtons}> 
+               Log com o Google
+              </Text>
+            </TouchableOpacity>
+        
+          </View>
+          <View>
+            <TouchableOpacity style={flexStyleLogin.loginButtons }>
+              <Text style={flexStyleLogin.textButtons}> 
+                 Log com a Apple
+              </Text>
+            </TouchableOpacity>
+        
+          </View>
+        <View style={{borderBottomColor: 'black', borderBottomWidth: 1, backgroundColor:"red"}} />
+        </View>
+        <View>
+          
+        </View>
     </View>
     
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    fontFamily: "sans-serif",
-    display: 'flex',
-    backgroundColor: '#000',
-    justifyContent: "center",
-    flexDirection: "row",
-    alignItems: 'center',
-    height: "100%",
-    gap: 10 
-  },
-  
-});
-
---
+++ styles.js
@@ -0,0 +1,58 @@
+import { StyleSheet } from "react-native"
+
+/*Criando as cores que irei mais utilizar no projeto*/
+export const globalColors = StyleSheet.create({
+    blue: {
+        color: '#0098FF',
+    },
+    black: {
+        color: "#000000"
+    },
+    white: {
+        color: "#FFFFFF"
+    }
+
+})
+
+//Padronizando as fontes que irei utilizar
+export const textStyle = StyleSheet.create({
+    bigSize: {
+        fontSize: 42,
+        fontWeight: "900"
+    },
+    mediumSize: {
+        fontSize: 21,
+        fontWeight:'900'
+    }
+})
+
+/*Criando os flex container */
+export const flexStyleLogin = StyleSheet.create({
+    flexStyleContainer: {
+        display: "flex",
+        flex:1,
+        flexDirection: 'column',
+        
+    },
+
+    formStyle: {
+        display: 'flex',
+        gap:20,
+        // backgroundColor: "red",
+        marginVertical: '50%',
+        marginHorizontal: 40
+
+    },
+
+    loginButtons: {
+        // width: "50%",
+        padding: 10,
+        borderWidth: 1,
+        borderRadius: 8
+    },
+    
+    textButtons: {
+        textAlign: "center",
+        fontSize: 19,
+    }
+})
